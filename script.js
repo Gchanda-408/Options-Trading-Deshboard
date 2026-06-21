b49903e154d65a268165642d0cd4ce840c9d09b9
Gchanda-408 <gchanda2011@gmail.com>
2026-06-21 19:22:26 +0530

    Add loading messages for NIFTY and BANKNIFTY

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("p")[0].innerText = "Loading NIFTY...";
+    document.querySelectorAll("p")[1].innerText = "Loading BANKNIFTY...";
+    document.querySelectorAll("p")[2].innerText = "WAIT";
+});
